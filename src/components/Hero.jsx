808317a44886cfb2612730304788825cdddc0b94
Paul Andrei Taranu <paulandreitaranu@gmail.com>
2025-10-09 12:59:42 +0100

    Finish Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,11 @@
-import { useRef, useState } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import { useEffect, useRef, useState } from 'react'
 import { TiLocationArrow } from 'react-icons/ti'
 import Button from './Button'
-import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1)
@@ -26,12 +30,36 @@ const Hero = () => {
                     ease: 'power1.inOut',
                     onStart: () => NEXT_VID_REF.current.play(),
                 })
-                
-                gsap.from('#current-video')
+
+                gsap.from('#current-video', {
+                    transformOrigin: 'center center',
+                    scale: 0,
+                    duration: 1.5,
+                    ease: 'power1.inOut',
+                })
             }
         },
         { dependencies: [currentIndex], revertOnUpdate: true },
     )
+
+    useGSAP(() => {
+        gsap.set('#video-frame', {
+            clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+            borderRadius: '0% 0% 40% 10%',
+        })
+        gsap.from('#video-frame', {
+            clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+            borderRadius: '0% 0% 0% 0%',
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '#video-frame',
+                start: 'center center',
+                end: 'bottom center',
+                scrub: true,
+            },
+        })
+    })
+
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`
 
     const upcomingVideoIndex = (currentIndex % TOTAL_VIDEOS) + 1
@@ -43,8 +71,23 @@ const Hero = () => {
 
     const handleVideoLoad = () => setLoadedVideos((prev) => prev + 1)
 
+    useEffect(() => {
+        if (loadedVideos == TOTAL_VIDEOS - 1) {
+            setIsLoading(false)
+        }
+    }, [loadedVideos])
+
     return (
         <section className=''>
+            {isLoading && (
+                <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50'>
+                    <div className='three-body'>
+                        <div className='three-body__dot'></div>
+                        <div className='three-body__dot'></div>
+                        <div className='three-body__dot'></div>
+                    </div>
+                </div>
+            )}
             <div
                 id='video-frame'
                 className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
