c26ce1ff2a664970d75852c349da29b85fedf537
Paul Andrei Taranu <paulandreitaranu@gmail.com>
2025-10-13 10:37:32 +0100

    Add About and AnimationTitle components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import About from './components/About'
 import Hero from './components/Hero'
 
 function App() {
     return (
         <main className='relative min-h-screen w-screen overflow-x-hidden'>
             <Hero />
-            <section className='z-0 min-h-screen bg-blue-300'></section>
+            <About />
+            <section className='h-[1900px]'>lormem ipsum</section>
         </main>
     )
 }
