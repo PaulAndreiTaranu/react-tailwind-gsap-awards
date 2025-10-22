cf3d9fcb8553ac520d2a2e5faa6b734857451ed0
Paul Andrei Taranu <paulandreitaranu@gmail.com>
2025-10-22 13:24:42 +0100

    Add Features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from './components/About'
+import Features from './components/Features'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
@@ -8,7 +9,7 @@ function App() {
             <Navbar />
             <Hero />
             <About />
-            <section className='h-[1900px]'>lormem ipsum</section>
+            <Features />
         </main>
     )
 }
