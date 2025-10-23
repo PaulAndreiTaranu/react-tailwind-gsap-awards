788e3b7b49c8503bd8d3ebfe8b781ea0b23cad4c
Paul Andrei Taranu <paulandreitaranu@gmail.com>
2025-10-23 14:07:02 +0100

    Add Story

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 import { useEffect, useRef } from 'react'
@@ -17,34 +18,34 @@ const AnimatedTitle = ({ title, containerClass }) => {
                 },
             })
 
-            titleAnimation.to('.animated-word', {
-                opacity: 1,
-                transform: 'translate3d(0,0,0) rotateY(0deg) rotateX(0deg)',
-                ease: 'power2.inOut',
-                stagger: 0.02,
-            })
+            titleAnimation.to(
+                '.animated-word',
+                {
+                    opacity: 1,
+                    transform: 'translate3d(0,0,0) rotateY(0deg) rotateX(0deg)',
+                    ease: 'power2.inOut',
+                    stagger: 0.02,
+                },
+                0,
+            )
         }, containerRef)
+
+        return () => ctx.revert() // Clean up on unmount
     }, [])
 
     return (
-        <div ref={containerRef} className={`animated-title ${containerClass}`}>
-            {title.split('<br />').map((line, index) => {
-                return (
-                    <div
-                        key={index}
-                        className='flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3'>
-                        {line.split(' ').map((word, i) => {
-                            return (
-                                <span
-                                    key={i}
-                                    className='animated-word'
-                                    dangerouslySetInnerHTML={{ __html: word }}
-                                />
-                            )
-                        })}
-                    </div>
-                )
-            })}
+        <div ref={containerRef} className={clsx('animated-title', containerClass)}>
+            {title.split('<br />').map((line, index) => (
+                <div key={index} className='flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3'>
+                    {line.split(' ').map((word, idx) => (
+                        <span
+                            key={idx}
+                            className='animated-word'
+                            dangerouslySetInnerHTML={{ __html: word }}
+                        />
+                    ))}
+                </div>
+            ))}
         </div>
     )
 }
